924140187e439792d60080eb5f73c43817caac7d
Gianluca <gianlucablancato@yahoo.it>
2022-05-21 16:10:24 +0200

    create func to count live cells and func to get live grid

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,8 +1,45 @@
-import { getGrid } from "./helper/functions";
+import { useEffect, useState } from "react";
+
+import { getLiveGrid } from "./helper/functions";
 
 const Grid = () => {
-  console.log(getGrid(10, 10));
-  return <h1>Gird</h1>;
+  const [grid, setGrid] = useState(getLiveGrid());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setGrid(getLiveGrid());
+    }, 2000);
+  }, [grid]);
+
+  const renderGrid = () => {
+    return grid.map((r, i) =>
+      r.map((_, j) => (
+        <div
+          key={`${i}-${j}`}
+          style={{
+            width: "2rem",
+            height: "2rem",
+            backgroundColor: grid[i][j] === 1 ? "red" : "white",
+            border: "1px solid black",
+          }}
+        ></div>
+      ))
+    );
+  };
+
+  return (
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}
+      >
+        <div
+          style={{ display: "grid", gridTemplateColumns: `repeat(20,2rem)` }}
+        >
+          {renderGrid()}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Grid;
--- src/helper/functions.ts
+++ src/helper/functions.ts
@@ -7,11 +7,52 @@ const createGrid = (columns: number, rows: number): TGrid => {
 };
 
 // function to fill with 0 and 1 every square of our grid
-const getGrid = (columns: number, rows: number): TGrid => {
+const fillGrid = (columns: number, rows: number): TGrid => {
   const grid = createGrid(columns, rows).map((x) =>
     x.map((y) => (y = Math.round(Math.random())))
   );
   return grid;
 };
 
-export { getGrid };
+const getLiveGrid = (): TGrid => {
+  const grid = fillGrid(20, 20);
+  const nextGrid = [...grid];
+  grid.map((r, i) =>
+    r.map((c, j) => {
+      //if statement to avoid crash on corner/edge
+      if (i === 0 || j === 0 || i === 20 - 1 || j === 20 - 1) {
+        nextGrid[i][j] = grid[i][j];
+      } else {
+        let neighbors = countNeighbors(grid, i, j);
+        let currentPosition = grid[i][j];
+        // below rules condition to switch cell's state
+        if (currentPosition === 0 && neighbors === 3) {
+          nextGrid[i][j] = 1;
+        } else if (currentPosition === 1 && (neighbors < 2 || neighbors > 3)) {
+          nextGrid[i][j] = 0;
+        } else {
+          nextGrid[i][j] = grid[i][j];
+        }
+      }
+    })
+  );
+  return nextGrid;
+};
+
+// function to count live cells
+const countNeighbors = (
+  grid: TGrid,
+  indexRow: number,
+  indexCol: number
+): number => {
+  let initialState = 0;
+  initialState = -grid[indexRow][indexCol];
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      initialState += grid[indexRow + i][indexCol + j];
+    }
+  }
+  return initialState;
+};
+
+export { getLiveGrid, countNeighbors };
